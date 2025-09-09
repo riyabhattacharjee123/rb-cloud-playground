0a495a8e7d4a9a7e8ccc73b0de8203b627d082cf
Riya Bhattacharjee <78217955+riyabhattacharjee123@users.noreply.github.com>
2025-09-09 12:55:13 +0000

    Day 2: added hello.sh Bash script

---
+++ scripts/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Simple Bash script for Day 2
+
+echo "Hello, Riya 🚀"
+echo "Today is: $(date)"
+echo "You are inside directory: $(pwd)"
+echo "Listing files in the current directory:"
+ls -la
+echo "Script execution completed!"
